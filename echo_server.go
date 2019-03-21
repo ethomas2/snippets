cab29476ef343523281766be671e6464d5727dfe
Evan Thomas <evan@tempoautomation.com>
2019-03-21 12:43:56 -0700

    Add file to echo server

--- echo_server.go
+++ echo_server.go
@@ -5,21 +5,62 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
 	var port int
+	var filename string
 	flag.IntVar(&port, "port", 8000, "The port to listen on (default 8000)")
+	flag.StringVar(&filename, "file", "", "Optional. A file to echo")
 	flag.Parse()
+
+	var filecontent string
+	var err error
+	if filename != "" {
+		filecontent, err = readFile(filename)
+		if err != nil {
+			fmt.Printf("\033[31merr %s\n\033[m", err)
+			os.Exit(1)
+		}
+	}
+
 	s := http.Server{
 		Addr: fmt.Sprintf(":%d", port),
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			s, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				fmt.Printf("\033[31merr %s\n\033[m", err)
+			var s string
+			if filename != "" {
+				s = filecontent
+
+			} else {
+				bytes, err := ioutil.ReadAll(r.Body)
+				s = string(bytes)
+				if err != nil {
+					fmt.Printf("\033[31merr %s\n\033[m", err)
+					return
+				}
 			}
-			fmt.Fprintln(w, string(s))
+			fmt.Fprint(w, string(s))
 		}),
 	}
 	fmt.Println(s.ListenAndServe())
+}
+
+func readFile(filename string) (string, error) {
+	filename = strings.Trim(filename, " ")
+	var bytes []byte
+	var err error
+	if filename != "-" {
+		bytes, err = ioutil.ReadFile(filename)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		bytes, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return "", err
+		}
+	}
+	return string(bytes), nil
 }
