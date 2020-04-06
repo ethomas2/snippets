ed38581b19f942e66a65c38bff6960dc4a207bc1
Evan Thomas <evan@tempoautomation.com>
2020-04-05 23:25:57 -0500

    Add cors to echo server

--- echo_server.go
+++ echo_server.go
@@ -12,8 +12,10 @@ import (
 func main() {
 	var port int
 	var filename string
+	var cors bool
 	flag.IntVar(&port, "port", 8000, "The port to listen on (default 8000)")
 	flag.StringVar(&filename, "file", "", "Optional. A file to echo")
+	flag.BoolVar(&cors, "cors", false, "Whether to respond to CORS headers")
 	flag.Parse()
 
 	var filecontent string
@@ -29,6 +31,30 @@ func main() {
 	s := http.Server{
 		Addr: fmt.Sprintf(":%d", port),
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if cors {
+				origins := r.Header["Origin"]
+				var origin string
+				// try to avoid setting origin to * directly bc
+				// chrome complains about setting origin to *
+				// if request.credentials has been set to
+				// 'include'
+				// https://developer.mozilla.org/en-US/docs/Web/API/Request/credentials
+				if len(origins) > 0 {
+					origin = origins[0]
+				} else {
+					origin = "*"
+				}
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Credentials
+				w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+				reqHeaders := r.Header["Access-Control-Request-Headers"]
+				if len(reqHeaders) > 0 {
+					w.Header().Set(
+						"Access-Control-Allow-Headers",
+						strings.Join(reqHeaders, ","))
+				}
+			}
 			var s string
 			if filename != "" {
 				s = filecontent
